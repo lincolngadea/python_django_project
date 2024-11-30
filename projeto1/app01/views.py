b020b5bd6c1f25a82cfdb9048c3a3fe60293db1a
lincolngadea <lincolngadea@gmail.com>
2024-11-30 19:51:06 -0300

    commit 07: cria página dinâmica

--- projeto1/app01/views.py
+++ projeto1/app01/views.py
@@ -15,7 +15,10 @@ def create(request):
         return render(request, 'user/criar.html', context= context)
     else:
         form = UserForm(request.POST)
-        if form.is_valid():
-            print(form)
-            form.save()
-    return render(request, 'user/index.html')
+        if form.is_valid():            
+            context ={
+                'nome': form.cleaned_data['nome'],
+                'telefone': form.cleaned_data['telefone'],
+                'email': form.cleaned_data['email']
+            }
+            return render(request, 'user/index.html', context= context)
