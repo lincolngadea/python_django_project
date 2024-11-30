f33dbec92b756ffe6b987b8a549376053fdb6ab3
lincolngadea <lincolngadea@gmail.com>
2024-11-30 19:26:50 -0300

    commit 06: inicia formulários usando Jinja

--- projeto1/app01/views.py
+++ projeto1/app01/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 def index(request):
     return render(request, 'user/index.html')
 
 def create(request):
-    return render(request, 'user/criar.html')
+
+    if request.method == 'GET':
+        form = UserForm()
+
+        context = {
+            'form': form,
+        }
+        return render(request, 'user/criar.html', context= context)
+    else:
+        form = UserForm(request.POST)
+        if form.is_valid():
+            print(form)
+            form.save()
+    return render(request, 'user/index.html')
